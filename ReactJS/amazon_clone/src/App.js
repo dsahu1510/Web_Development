e5b91fb600c4b822860873966f1c7728814eff48
Devyani <devyanisahu1@gmail.com>
2022-03-06 20:25:31 +0530

    added

---
+++ ReactJS/amazon_clone/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from './Header.js';
+import Home from './Home.js';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Header></Header>
+      < Home ></Home>
+
+    </div>
+
+  );
+}
+
+export default App;
---
+++ ReactJS/amazon_clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB_ljPZ4sM4CKBbzg-9Zkj45p4atizFDaU",
+    authDomain: "challenge-fe83c.firebaseapp.com",
+    projectId: "challenge-fe83c",
+    storageBucket: "challenge-fe83c.appspot.com",
+    messagingSenderId: "959163810625",
+    appId: "1:959163810625:web:ce1344e76db066720fd7c0",
+    measurementId: "G-NSRKMR0GFL"
+  };
