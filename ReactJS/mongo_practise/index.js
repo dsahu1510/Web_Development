9c853c12a5c9e0ab7721f430bc22136001b14731
Devyani <devyanisahu1@gmail.com>
2022-04-01 20:57:38 +0530

    adding

--- ReactJS/mongo_practise/index.js
+++ ReactJS/mongo_practise/index.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose');
 var Schema=mongoose.Schema;
  var UsersSchema = new Schema({
-      empId: {
+      id: {
+          type:Number
+      },
+      name: {
           type:String
       },
-      empName: {
-          type:String
-      },
-      empDesg: {
+      address: {
           type:String
       }
  });
 
- module.exports=mongoose.model('users',UsersSchema);
+ module.exports=mongoose.model('user',UsersSchema);
--- ReactJS/mongo_practise/users.js
+++ ReactJS/mongo_practise/users.js
@@ -3,7 +3,7 @@ var app =express();
 var mongoose = require('mongoose');
 var index = require('./index');
 
-mongoose.connect("mongodb://localhost/L&T",(err) =>{
+mongoose.connect("mongodb://localhost/myntra",(err) =>{
     if(err){
         console.log("error",err);
     }
@@ -12,7 +12,7 @@ mongoose.connect("mongodb://localhost/L&T",(err) =>{
     }
 })
 
-app.get('/Employee', (req,res) => {
+app.get('/users', (req,res) => {
 index.find({},(err,docs) => {
     console.log(docs);
     res.send(docs);
@@ -20,7 +20,7 @@ index.find({},(err,docs) => {
 
 })
 
-app.listen(8000, () => {
+app.listen(8011, () => {
     console.log("server started!");
 })
 // npm i express mongoose
